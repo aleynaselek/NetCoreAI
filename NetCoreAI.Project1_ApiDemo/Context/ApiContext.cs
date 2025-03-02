556b55a66a76f4f2cc064851cd1d9032f7c76e2a
aleynaflo <selekaleyna@gmail.com>
2025-03-02 19:53:39 +0300

    Add project files.

---
+++ NetCoreAI.Project1_ApiDemo/Context/ApiContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NetCoreAI.Project1_ApiDemo.Enitities;
+
+namespace NetCoreAI.Project1_ApiDemo.Context
+{
+    public class ApiContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=DESKTOP-OTC2IOU\\SQLEXPRESS;Initial Catalog=ApiAIDB;Integrated Security=True;TrustServerCertificate=True;");
+        }
+
+        public DbSet<Customer> Customers { get; set; }
+    }
+}
---
+++ NetCoreAI.Project2_ApiConsumeUI/Controllers/CustomerController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NetCoreAI.Project2_ApiConsumeUI.Dtos;
+using Newtonsoft.Json;
+
+namespace NetCoreAI.Project2_ApiConsumeUI.Controllers
+{
+    public class CustomerController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public CustomerController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult> CustomerList()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7243/api/customers");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultCustomerDto>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+    }
+}
